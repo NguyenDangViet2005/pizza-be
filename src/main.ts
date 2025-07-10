108256aafa9e274faf381c7a5c1f89a00712d11d
nguyen dang viet <vietnguyen.1022005@gmail.com>
2025-07-10 22:41:26 +0700

    open cors for call api

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ declare const module: any
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
+  // Mở CORS cho localhost:3000
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, // Nếu bạn dùng cookie hoặc xác thực
+  })
   await app.listen(process.env.PORT ?? 8080)
 
   if (module.hot) {
